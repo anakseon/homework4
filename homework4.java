b943d95fcb0dc188f529ccce9e50e55ed8543fab
anakseon <127014911+anakseon@users.noreply.github.com>
2023-07-02 10:03:07 +0300

    Add files via upload
    домашнее задание к семинару

---
+++ homework4.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+public class homework4 {
+     /**
+     * @param args
+     * @param j TODO
+     */
+    public static void main(String[] args, int j) {
+         ArrayList<Integer> number = new ArrayList<Integer>(10);
+        System.out.print(number);
+        for (int i=0;i<number.size();i++){      
+       int even=number.get(i)%2;       
+        if (even==0){
+            number.remove(i);
+            System.out.print(number);
+        } 
+    int[] a;
+    Object min = a[0];
+Object mass;
+for (var n = 0; n < 10; n++) {
+    if (a[n] < min) {
+       min = a[n];
+    } 
+System.out.println(min);
+} 
+    int[] b;
+    int max = b[0] + 1;
+for (int j = 0; j < 10; j++) {
+    if (b[j]> max) {
+       max = b[j];
+    } 
+System.out.println(max);
+} 
+int m;
+if (m > 0)
+{
+    double sum = 0;
+    m++; 
+    double[] c;
+    sum += c[m];
+    double average = sum / 10;
+}
+    }
+
+}
+}
